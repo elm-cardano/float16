d515a89287e2c463c292f3cd7b9433921f9ec960
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2026-04-15 21:19:07 +0200

    Add more detailed benchmark for pure arithmetic implementations
    Signed-off-by: Matthieu Pizenberg <matthieu.pizenberg@gmail.com>

--- bench/src/Bench.elm
+++ bench/src/Bench.elm
@@ -9,8 +9,10 @@ module Bench exposing
     , lookupTable_encode
     , pureArithmetic_decode
     , pureArithmetic_encode
+    , pureArithmetic_encode_batch
     , scaleFloor_decode
     , scaleFloor_encode
+    , scaleFloor_encode_batch
     , successiveBit_decode
     , successiveBit_encode
     )
@@ -43,6 +45,33 @@ testFloat =
 testUint16 : Int
 testUint16 =
     0x4A30
+
+
+{-| Diverse encode inputs spanning all float16 regions.
+Exercises different loop iteration counts in frexp/scaleToRange:
+  - near 1.0: 0 iterations
+  - large values (65504): ~15 halvings
+  - tiny subnormals (5.96e-8): ~24 doublings
+  - negative, overflow, zero
+-}
+batchFloats : List Float
+batchFloats =
+    [ 0
+    , -0.0
+    , 1.0
+    , 1.5
+    , -0.25
+    , 0.375
+    , 1.001
+    , 0.99951
+    , 12.375
+    , 82.125
+    , 65504.0
+    , 0.000061035
+    , 5.96046e-8
+    , 1.0e-9
+    , 12547414.0
+    ]
 
 
 
@@ -127,6 +156,20 @@ scaleFloor_encode () =
 scaleFloor_decode : () -> Float
 scaleFloor_decode () =
     ScaleFloor.decode testUint16
+
+
+
+-- Batch: PureArithmetic vs ScaleFloor over diverse inputs
+
+
+pureArithmetic_encode_batch : () -> List Int
+pureArithmetic_encode_batch () =
+    List.map PureArithmetic.encode batchFloats
+
+
+scaleFloor_encode_batch : () -> List Int
+scaleFloor_encode_batch () =
+    List.map ScaleFloor.encode batchFloats
 
 
 
